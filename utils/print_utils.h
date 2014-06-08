9b995dc5d9eed14c1f96b51fbfc3fc56abf595f9
Prashanth Kannan <prashrock@gmail.com>
2014-06-08 15:14:46 -0700

    DP Program = Minimum Edit Distance (06/08/2014)

--- utils/print_utils.h
+++ utils/print_utils.h
@@ -13,4 +13,18 @@ static inline void print_int_array(const int *arr, int n)
 	printf("\n");
 }
 
+static inline void print_int_matrix(int n, int m, int mat[n][m])
+{
+	int i, j;
+	/* Print the Matrix */
+	printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+			printf ("%3d ", mat[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+}
+
 #endif //_PRINT_UTILS_API_
--- utils/scan_utils.h
+++ utils/scan_utils.h
@@ -1,30 +1,50 @@
 #ifndef _SCAN_UTILS_API_
 #define _SCAN_UTILS_API_
 #include <stdio.h>
-#include <stdbool.h>       /* bool, true, false */
-#include <stdlib.h>        /* atoi  */
-#include "compiler_api.h"  /* unlikely */
+#include <stdbool.h>        /* bool, true, false */
+#include <stdlib.h>         /* atoi  */
+#include <limits.h>         /* INT_MAX */
+#include "compiler_api.h"   /* unlikely */
+
+#define MAX_INT_DIGITS (10) /* 4 Billion = 10 Digits */
+
+/* Expects a dynamic container if max_len == -1           *
+ * Otherwise inputs max_len-1 characters and appends (\0) *
+ * Returns -1 upon failure and strlen upon success        */
+static inline int input_generic_api(char c[], int max_len)
+{
+	int i = 0;
+	if(max_len == -1) max_len = INT_MAX;
+	while((c[i++] = getchar()) != '\n') if(i > max_len-1) break;
+	c[i] = '\0';
+	if(c[0] == 0 || c[0] == '\n') return -1;
+	else                          return i;
+}
 
 static inline bool input_integer(int *n)
 {
-	char c[50];
-	int i = 0;
-	while((c[i++] = getchar()) != '\n') if(i > 40) break;
-	c[i] = '\0';
-	*n =  atoi(c);
-	if(*n == 0 && c[0] != '0') return false;
-	else                       return true;
+	char c[MAX_INT_DIGITS+1];
+	int i = input_generic_api(c, MAX_INT_DIGITS+1);
+	if(i > 0) {
+		*n =  atoi(c);
+ 		return true;
+	}
+	else return false;
 }
 
+/* Use generic input API and trim '\n' in the end */
+static inline int input_string(char c[], int max_len)
+{
+	int len = input_generic_api(c, max_len);
+	if(len > 0 && c[len-1] == '\n'){
+		c[len-1] = c[len];
+		len--;
+	}
+	return len;
+}
 static inline bool input_unsigned_integer(unsigned int *n)
 {
-	char c[50];
-	int i = 0;
-	while((c[i++] = getchar()) != '\n') if(i > 40) break;
-	c[i] = '\0';
-	*n =  atoi(c);
-	if(*n == 0 && c[0] != '0') return false;
-	else                       return true;
+	return input_integer((int *) n);
 }
 
 #endif //_SCAN_UTILS_API_
