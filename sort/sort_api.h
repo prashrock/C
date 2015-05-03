c6c5ea43d69802f4df3cfae53ad77053863f270e
Prashanth Kannan <prashrock@gmail.com>
2015-05-03 15:55:25 +0000

    Sort API - mode, lowest/highest idx: 05/02/2015

--- sort/sort_api.h
+++ sort/sort_api.h
@@ -76,4 +76,95 @@ static inline bool sort_api(enum SORT_TYPE st,
 	return ret;
 }
 
+/* Given a sorted dynamic array, check if elements are in sorted order    *
+ * If total of elements is provided, check if the elements in the dynamic *
+ * array sum up to this value                                             */
+static inline bool validate_sort_order(const int *array, int n,
+								unsigned long long total)
+{
+	int i;
+	bool total_check = true;
+	if(n == 0)             return true; /* Nothing to validate */
+	if(total == 0)         total_check = false;
+	total -= array[0];
+	for(i = 1; i < n; i++)
+	{
+		if(array[i] < array[i-1])
+		{
+			printf("Error: Sort Invariant not met  - ");
+			printf("%d found before %d\n", array[i-1], array[i]);
+			return false;
+		}
+		total -= array[i];
+	}
+	/* Avoid checking if user wanted to do total checks until now */
+	if(total_check && total) {
+		printf("Error: SUM(Array) < User_Total (by %llu)\n", total);
+		return false;
+	}
+	return true;
+}
+
+
+/* Find element which re-occurs the most. If more than one mode   *
+ * present, return the first occuring mode                        */
+static inline bool find_mode_in_sorted_array(const int *array, int n,
+											int *mode, int *mode_freq)
+{
+	int i, gmode, lmode;
+	unsigned gmode_freq, lmode_freq;
+	if(n == 0)             return true;  /* Nothing to validate */
+	if(array == NULL)      return false; /* Empty array - errro */
+	/* Make sure the Input array is already sorted              */
+	if(validate_sort_order(array, n, 0) == false) {
+		return false;
+	}
+	gmode = lmode = array[0];
+	gmode_freq = lmode_freq = 1;
+	for(i = 1; i < n; i++)
+	{
+		if(array[i] == lmode) lmode_freq++;
+		else {
+			if(lmode_freq > gmode_freq) {
+				gmode_freq = lmode_freq;
+				gmode = lmode;
+			}
+			lmode = array[i];
+			lmode_freq = 1;
+		}
+	}
+	if(mode)      *mode = gmode;
+	if(mode_freq) *mode_freq = gmode_freq;
+	return true;
+}
+
+/* Find first or last occurence of particular number in the given *
+ * sorted array (invariant).                                      *
+ * Time Complexity = O(log n) very similar to binary search       */
+static inline int find_first_or_last_idx(const int *array, int n,
+										  int k, bool first)
+{
+	int low = 0, high = n-1, mid;
+	int idx = -1;
+	if(n == 0 || array == NULL) /* Sanity Checks                  */
+		return idx;
+	while(low <= high)
+	{
+		bool go_low;
+		mid = low + (high - low) / 2;
+		if(array[mid] < k)      /* Search only in right sub-tree  */
+			go_low = false;
+		else if(array[mid] > k) /* Search only in left sub-tree   */
+			go_low = true;
+        /* Found element. Binary search ends here, we go further  */
+		else {
+			idx = mid;
+			go_low = first;
+		}
+		/* Based on decisions above, adjust low or high           */
+		if(go_low) high = mid - 1;
+		else       low  = mid + 1;
+	}
+	return idx;
+}
 #endif //_SORT_API_H_
--- sort/sort_test.c
+++ sort/sort_test.c
@@ -26,73 +26,98 @@
 #include "shell_sort.h"       /* Shell sort API's */
 #include "merge_sort.h"       /* Merge sort API's + inversion cnt API */
 #include "sort_api.h"         /* Wrapper API's around sort */
-dq_t *dyq;
-dq_stats_t dq_stat_val;
+static dq_t *dyq;
+static int *sort_array;       /* To convert between queue and array */
+static int sort_array_idx = 0;/* Keep track of sort array index */
 enum SORT_TYPE sort_type = SELECTION_SORT;
+#define MAX_RAND 10
 
-uint64_t tmp;
+
+/* Dynamic Queue to array converter */
+static void copy_queue_elem_to_array(int *val)
+{
+	sort_array[sort_array_idx++] = *val;
+}
+
+/* Generate n random integers in the range [0, MAX_RAND) and add them to  *
+ * the provided dynamic array implementation (dy_queue)                   */
+static bool generate_random_integers(dq_t *dyq, int n, int max_val,
+									 unsigned long long *total_ptr)
+{
+	unsigned long long total = 0;
+	int i, rand_val;
+	for(i = 0; i < n; i++)     /* Add n random integers to container  */
+	{
+		rand_val = get_rand_int_limit(max_val);
+		dq_enq(dyq, rand_val);
+		total += rand_val;
+	}
+	if(total_ptr) *total_ptr = total;
+	return true;
+}
+
+/* A hack to look at elements as an array - assume container does     *
+ * not grow or shrink right now (not multi-thread safe)               */
+static bool validate_sort_order_wrapper(dq_t *dyq, int n, unsigned
+										long long total)
+{
+	bool ret;
+	sort_array = malloc(sizeof(int[dq_count(dyq)]));
+	if(sort_array == NULL)                    return false;
+	dq_iterator(dyq, copy_queue_elem_to_array);
+	ret = validate_sort_order(sort_array, n, total);
+	free(sort_array);
+	sort_array_idx = 0;
+	return ret;
+}
+	
 /* Common wrapper to invoke sort, measure time and print elements */
-static void sort_test(bool inprint, bool outprint)
+static bool sort_test(bool inprint, bool outprint)
 {
 	struct time_api_t ta;
 	int n = dq_count(dyq);
 	const char *sname;
+
 	if(outprint)     sname = sort_name[sort_type];
 	else             sname = NULL;
 					
 	if (n == 0)
 	{
 		printf("Info: Array empty, nothing to sort\n");
-		return;
+		return true;
 	}
 	if(inprint){
 		printf("Elements before sorting: ");
 		dq_iterator(dyq, dq_print_element);
 		printf("\n");
 	}
-	if(rt_measure_start(&ta, false) == false) return;
+	if(rt_measure_start(&ta, false) == false) return false;
 	sort_api(sort_type, dyq->elems, dq_count(dyq));
-	if(rt_measure_end(&ta, false) == false)   return;
+	if(rt_measure_end(&ta, false) == false)   return false;
 	time_print_api(&ta, sname);
 	if(inprint){
 		printf("Elements after sorting: ");
 		dq_iterator(dyq, dq_print_element);
 		printf("\n");
 	}
+	if(n != dq_count(dyq)) {
+		printf("Error: Missing elements in sorted array: ");
+		printf("Expected %d, found %d elements\n", n, dq_count(dyq));
+		return false;
+	}
+	
+	return validate_sort_order_wrapper(dyq, n, 0);
 }
-/* Handle test cases such as missing/overwritten elements after sort and  *
- * Sort invariant failure */
+
 static void rand_sort_test(int n, bool inprint, bool outprint)
 {
 	unsigned long long total = 0;
-	int i, rand_val, new_val;
-	while(dq_deq(dyq, &rand_val));
-	for(i = 0; i < n; i++)
-	{
-		rand_val = get_rand_int_limit(n);
-		dq_enq(dyq, rand_val);
-		total += rand_val;
-	}
-	sort_test(inprint, outprint);
-	rand_val = 0;
-	for(i = 0; i < n; i++)
-	{
-		if(dq_deq(dyq, &new_val) == false)
-			break;
-		if(new_val < rand_val)
-		{
-			printf("Error: Sort Invariant not met  - ");
-			printf("%d found before %d\n",
-				   rand_val, new_val);
-			return;
-		}
-		rand_val = new_val;
-		total -= rand_val;
-	}
-	if(total)
-		printf("Error: Elements lost after sort (tot=%llu)\n",
-			total);
-	else if(outprint == true)
+	int val;
+	while(dq_deq(dyq, &val)); /* Clear any inputs in container            */
+	generate_random_integers(dyq, n, MAX_RAND, &total);
+	if(sort_test(inprint, outprint) == false)
+		return;
+	else if(validate_sort_order_wrapper(dyq, n, total) == true)
 		printf("'%s' successful on %d random elements\n",
 		   sort_name[sort_type], n);
 }
@@ -148,10 +173,12 @@ static void print_help_string()
 	printf("\t          p - Print Array (sorted or unsorted)\n");
 	printf("\t          n - Print Size information of Array\n");
 	printf("\t      sort* - Sort user inputted integers\n");
+	printf("\t       mode - Calc mode of sorted Array\n");
+	printf("\t      occur - Find 1st/last occur in sorted Array\n");
 	printf("\t  rand_sort - Generate & Sort 'n' random integers\n");
 	printf("\t    pr_sort - Generate, Sort, Print 'n' random integers\n");
 	printf("\t   cmp_sort - Compare sorts with 'n' rand integers\n");
-	printf("\t       mode - Select type of sort (Insert, Quick, etc.)\n");
+	printf("\t  sort_type - Select type of sort (Insert, Quick, etc.)\n");
 //	printf("\t         t2 - create '2' threads to sort 1M random nos\n ");
 	printf("\t       NOTE - * CMDS take optional loop_count (sort 2)\n");
 }
@@ -160,14 +187,56 @@ static void multi_char_CLI(const char *c)
 {
 	int lp = -1;
 	const char *space;
+	bool lp_found = false;
 	if((space = my_strstr_with_strlen(c, " ")))
 	{
 		lp = atoi(space + 1);
 		printf("lp = %d\n", lp);
+		lp_found = true;
 	}
 	if(strncmp(c, "sort", strlen("sort")) == 0)
 	{
 		do { sort_test(true, true);} while(lp > 0 && lp--);
+	}
+	if(strncmp(c, "mode", strlen("mode")) == 0)
+	{
+		int mode, mode_freq;
+		sort_array = malloc(sizeof(int[dq_count(dyq)]));
+		if(sort_array != NULL) {
+			dq_iterator(dyq, copy_queue_elem_to_array);
+			if(find_mode_in_sorted_array(sort_array, dq_count(dyq),
+										 &mode, &mode_freq) == false)
+				printf("Error: Mode calculation failed.\n");
+			else
+				printf("Mode of %d elements = %d (freq=%d)\n",
+					   dq_count(dyq), mode, mode_freq);
+		}
+		free(sort_array);
+		sort_array_idx = 0;
+	}
+	if(strncmp(c, "occur", strlen("occur")) == 0)
+	{	
+		int first_idx, last_idx;
+		if(lp_found == false)
+			printf("Usage: occur <val_to_search>\n");
+		else {
+			sort_array = malloc(sizeof(int[dq_count(dyq)]));
+			if(sort_array != NULL) {
+				dq_iterator(dyq, copy_queue_elem_to_array);
+			}
+			first_idx = find_first_or_last_idx(sort_array,
+											   dq_count(dyq),
+											   lp, true);
+			last_idx = find_first_or_last_idx(sort_array,
+											  dq_count(dyq),
+											  lp, false);
+			free(sort_array);
+			sort_array_idx = 0;
+			printf("First occurence of %d = %d (zero-based position)\n",
+				   lp, first_idx);
+			printf(" Last occurence of %d = %d (zero-based position)\n",
+				   lp, last_idx);
+		}
 	}
 	else if(strncmp(c, "rand_sort", strlen("rand_sort")) == 0)
 	{
@@ -190,19 +259,19 @@ static void multi_char_CLI(const char *c)
 		else
 			cmp_sort_test(lp);
 	}
-	else if(strncmp(c, "mode", strlen("mode")) == 0)
+	else if(strncmp(c, "sort_type", strlen("sort_type")) == 0)
 	{
 		if(lp == -1 || lp >= INVALID_SORT)
 		{
-			printf("Info: To set new sort mode, use 'mode <0-n>'\n");
-			printf("Info: Current sort_mode = '%s'\n", sort_name[sort_type]);
+			printf("Info: To set new sort type, use 'sort_type <0-n>'\n");
+			printf("Info: Current sort_type = '%s'\n", sort_name[sort_type]);
 			print_sort_mode_table();
 		}
 		else
 		{
-			printf("previous sort_mode = '%s'\n", sort_name[sort_type]);
+			printf("previous sort_type = '%s'\n", sort_name[sort_type]);
 			sort_type = lp;
-			printf("new sort_mode = '%s'\n",sort_name[sort_type]);
+			printf("new sort_type = '%s'\n",sort_name[sort_type]);
 		}
 	}
 }
@@ -260,6 +329,7 @@ static void single_char_CLI_with_num(const char *c)
 
 int main()
 {
+	init_rand();
 	dyq = dq_init();
 	print_help_string();
 	handle_CLI(single_char_CLI, single_char_CLI_with_num, multi_char_CLI);
