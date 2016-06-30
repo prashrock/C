9f36a6337f77d9334738cd1ac0c6957eba336565
Prashanth Kannan <prashrock@gmail.com>
2016-06-30 07:09:43 -0700

    Distributed code indentation

--- distributed/dist_test.c
+++ distributed/dist_test.c
@@ -1,4 +1,4 @@
-/* gcc -g -o dist_test dist_test.c */
+/* gcc -O3 -g -o dist_test dist_test.c */
 #include <stdio.h>
 #include <stdbool.h>       /* bool, true, false */
 #include <pthread.h>       /* Pthreads library */
@@ -19,72 +19,69 @@
 
 static void find_majority_test()
 {
-	int n, majority;
-	dq_t *dq = dq_init();
-	printf("Input Integer Array(press enter after each integer):\n");
-	while(1){
-		if(input_integer(&n))	dq_enq(dq, n);
-		else break;
-	}
-	printf("User entered %d integers:\n", dq_count(dq));
-	dq_iterator(dq, dq_print_element);
-	printf("\n");
-	if(boyer_moore_majority_vote(dq->elems, dq_count(dq), &majority))
-		printf("Majority Element = %d\n", majority);
-	else
-		printf("No Majority Found\n");
-	dq_destroy(dq);
+   int n, majority;
+   dq_t *dq = dq_init();
+   printf("Input Integer Array(press enter after each integer):\n");
+   while(1){
+      if(input_integer(&n))	dq_enq(dq, n);
+      else break;
+   }
+   printf("User entered %d integers:\n", dq_count(dq));
+   dq_iterator(dq, dq_print_element);
+   printf("\n");
+   if(boyer_moore_majority_vote(dq->elems, dq_count(dq), &majority))
+      printf("Majority Element = %d\n", majority);
+   else
+      printf("No Majority Found\n");
+   dq_destroy(dq);
 }
 static void print_help_string()
 {
-	printf("\n\nPress Enter to exit or follow below commands\n");
-	printf("\t        ? - Print this text again\n");
-	printf("\tmajority* - Boyer Moore Majority Vote Algorithm\n");
-	printf("\t NOTE - * CMDS take optional loop_count, eg majority 2\n");
+   printf("\n\nPress Enter to exit or follow below commands\n");
+   printf("\t        ? - Print this text again\n");
+   printf("\tmajority* - Boyer Moore Majority Vote Algorithm\n");
+   printf("\t NOTE - * CMDS take optional loop_count, eg majority 2\n");
 }
 static void multi_char_CLI(const char *c)
 {
-	int lp = 1;
-	const char *space;
-	if((space = my_strstr_with_strlen(c, " ")))
-	{
-		lp = atoi(space + 1);
-		if(lp == 0) lp = 1;
-		printf("lp = %d\n", lp);
-	}
-	if(strncmp(c, "majority", strlen("majority")) == 0)
-	{
-		printf("Find integer occuring for half or more times\n");
-		while(lp--) find_majority_test();
-	}
+   int lp = 1;
+   const char *space;
+   if((space = my_strstr_with_strlen(c, " ")))
+      {
+         lp = atoi(space + 1);
+         if(lp == 0) lp = 1;
+         printf("lp = %d\n", lp);
+      }
+   if(strncmp(c, "majority", strlen("majority")) == 0)
+      {
+         printf("Find integer occuring for half or more times\n");
+         while(lp--) find_majority_test();
+      }
 }
 static void single_char_CLI(const char *c)
 {
-	switch( *c )
-	{
-	case '?':
-		print_help_string();
-		break;
-	default:
-		break;
-	}
+   switch( *c ) {
+   case '?':
+      print_help_string();
+      break;
+   default:
+      break;
+      }
 }
 static void single_char_CLI_with_num(const char *c)
 {
-	//int x = atoi(c+1);
-	switch(*c)
-	{
-	case 't': case 'T':
-		//handle_multi_threaded_test(x, queue_test_thread, false);
-		printf("No Thread usage yet\n");
-		break;
-	default:
-		break;
-	}
+   switch(*c) {
+   case 't': case 'T':
+      //handle_multi_threaded_test(x, queue_test_thread, false);
+      printf("No Thread usage yet\n");
+      break;
+   default:
+      break;
+   }
 }
 int main()
 {
-	print_help_string();
-	handle_CLI(single_char_CLI, single_char_CLI_with_num, multi_char_CLI);
-	return 0;
+   print_help_string();
+   handle_CLI(single_char_CLI, single_char_CLI_with_num, multi_char_CLI);
+   return 0;
 }
--- distributed/election.h
+++ distributed/election.h
@@ -4,37 +4,30 @@
 
 #define INTEGER_DIV_CEIL(_divisor)  ((_divisor) - 1)
 
-/* Boyer Moore Majority Algorithm to find Majority element in a *
- * given input array. The majority element occurs half or more  *
- * times in the array. If two elements qualify for majority,i.e *
- * only 2 elements occur n/2 times each, then 1st is picked     *
- * Time Complexity = O(n), Space Complexity = O(1)              *
- */
+/* Boyer Moore Majority Algorithm to find Majority element   *
+ * in a given input array. The majority element occurs half  *
+ * or more times in the array. If two elements qualify for   *
+ * majority, i.e only 2 elements occur n/2 times each, then  *
+ * 1st is picked                                             *
+ * Time Complexity = O(n), Space Complexity = O(1)           */
 int boyer_moore_majority_vote(const int arr[], int n, int *result)
 {
-	int candidate, count = 0, i;
-	/* First Pass */
-	for(i = 0; i < n; i++)
-	{
-		if(count == 0)         	candidate = arr[i];
-		if(candidate == arr[i]) count++;
-		else                    count--;
-	}
-	/* Second Pass to verify if candidate is really majority or *
-	 *  simply one with non-zero count (occuring towards end)   */
-	count = 0;
-	for(i = 0; i < n; i++)
-		if(candidate == arr[i]) count++;
-	/* For odd number of elements, integer division will cause  *
-	 * error when determining n/2. For ex, 7/2 = 3,             *
-	 * therefore do count < ceil(n/2)                           */
-	if(count < ((n + INTEGER_DIV_CEIL(2)) / 2))
-		return false;
-	else
-	{
-		*result = candidate;
-		return true;
-	}
+   int candidate, count = 0, i;
+   /* First Pass */
+   for(i = 0; i < n; i++) {
+      if(count == 0)          candidate = arr[i];
+      if(candidate == arr[i]) count++;
+      else                    count--;
+   }
+   /* Second Pass to verify if candidate is really majority  *
+    * or simply one with non-zero count(occuring towards end)*/
+   for(count = 0, i = 0; i < n; i++)
+      if(candidate == arr[i]) count++;
+   /* For odd number of elements, integer division will      *
+    * cause error when determining n/2. For ex, 7/2 = 3,     *
+    * therefore do count < ceil(n/2)                         */
+   if(count < ((n + INTEGER_DIV_CEIL(2)) / 2)) { return false; }
+   else                  {  *result = candidate; return true;  }
 }
 
 #endif //_ELECTION_API_
